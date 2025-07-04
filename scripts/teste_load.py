d2d19ab1b4a7ca301564ed51257fcca63428391a
brjatoba92 <brjatoba92@gmail.com>
2025-07-04 15:45:38 -0300

    :sparkles: feat: Filter implemented and teste_load update

--- scripts/teste_load.py
+++ scripts/teste_load.py
@@ -1,6 +1,7 @@
 from src.etl.load_data import criar_spark_session, carregar_dados_vendas, carregar_dados_lojas, carregar_dados_produtos
 from src.etl.transform import preparar_dataset_completo
 from src.analysis.vendas_por_loja import calcular_agregados_por_loja, gerar_ranking_lojas
+from src.analysis.filter import filtrar_por_periodo, filtrar_por_categoria, filtrar_por_subcategoria, filtra_por_regiao
 
 spark = criar_spark_session()
 # ETL: Carregar os dados
@@ -19,3 +20,26 @@ df_ranking = gerar_ranking_lojas(df_agregado)
 df_completo.show(5, truncate=False)
 
 
+# Filtros
+"""
+Filtros para análise de vendas:
+1. Por período: Filtra as vendas dentro de um intervalo de datas.
+2. Por categoria: Filtra as vendas de produtos de uma categoria específica.
+3. Por subcategoria: Filtra as vendas de produtos de uma subcategoria específica.
+4. Por região: Filtra as vendas de lojas em uma região específica.
+
+"""
+df_filtrado = filtrar_por_periodo(df_completo, "2024-01-15", "2024-02-15")
+
+df_filtrado = filtrar_por_categoria(df_completo, "Ferragens")
+
+df_filtrado = filtrar_por_subcategoria(df_completo, "Pregos")
+
+df_filtrado = filtra_por_regiao(df_completo, "Nordeste")
+
+# Analise com os dados filtrados
+df_agregado_filtrado = calcular_agregados_por_loja(df_filtrado)
+df_ranking_filtrado = gerar_ranking_lojas(df_agregado_filtrado)
+
+# Exibir os dados filtrados e o ranking
+df_ranking_filtrado.show(10, truncate=False)
